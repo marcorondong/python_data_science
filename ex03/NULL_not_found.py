65a041d9a482f9ecd75b7fffdd822b3ad1bd8b71
Marco Rondon <marcorondong@hotmail.com>
2025-09-23 19:56:44 +0200

    FEAT: Added ex03

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,5 +1,29 @@
 def NULL_not_found(object: any) -> int:
-	if isinstance(object, None):
-		print(f"Nothing : {object} {type(object)}")
+	Nothing = None
+	# if isinstance(object, None):
+	# if object == Nothing:
+	if object is None:
+		print(f"Nothing: {object} {type(object)}")
+		return 0
+	# Here I need to check for NaN. NaN is not equal to itself, and it's a "Number"
 	elif isinstance(object, float):
-		print(f"Cheese : {object} {type(object)}")
+		# if object == float("NaN"):
+		if object != object:
+			print(f"Cheese: {object} {type(object)}")
+			return 0
+	elif isinstance(object,str):
+		if len(object) == 0:
+			print(f"Empty:{object} {type(object)}")
+			return 0
+	elif isinstance(object,bool):
+		# Can I do this? Or I need to do object == False
+		if not object:
+			print(f"Fake: {object} {type(object)}")
+			return 0
+	elif isinstance(object,int):
+		if object == 0:
+			print(f"Zero: {object} {type(object)}")
+			return 0
+	# else:
+	print("Type not Found")
+	return 1
