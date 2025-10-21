514064e45bd4f9338e780ad09ed01d9d1d11044c
Marco Rondon <marcorondong@hotmail.com>
2025-10-21 15:25:58 +0200

    CHORE: Added edge cases protection to function (still need protection in iterable)

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,5 +1,14 @@
 
-def ft_filter(func, iterable):
+def ft_filter(func=None, iterable=None):
+    # Check if func is a function, and if it's callable
+    if func is not None and not callable(func):
+        raise TypeError(f"'{type(func).__name__}' object is not callable")
+    # Check if iterable is actually an iterable (list, tuple, etc)
+    try:
+        _ = iter(iterable) # Underscore is used for variables I won't use
+    except TypeError:
+        raise TypeError(f"'{type(iterable).__name__}' object is not iterable")
+    # If function is None, then make it boolean
     if func is None:
         func = bool
     return [x for x in iterable if func(x)]
@@ -24,8 +33,10 @@ def extract_even(number):
 
 def main():
     numbers = [1, 3, 10, 45, 6, 50]
-    print(list(ft_filter(extract_even, numbers)))
+    whatever = 424242
+    # print(list(ft_filter(extract_even, numbers)))
     # print(list(ft_filter(lambda n: n % 2 == 0, numbers)))
+    print(list(ft_filter(lambda n: n % 2 == 0, whatever)))
 
 if __name__ == "__main__":
     main()
