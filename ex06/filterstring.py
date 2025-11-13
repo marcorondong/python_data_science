5671151fc34cc9e012d68f628878d22033f7b0bf
Marco Rondon <marcorondong@hotmail.com>
2025-11-13 16:39:33 +0100

    CHORE: Implemented part2 of ex06. Needs cleaning

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -8,10 +8,14 @@ def main():
         word_length = int(sys.argv[2])
         if not isinstance(text, str):
             raise TypeError("the arguments are bad")
-        word_list = text.split()
+        # word_list = text.split()
+        words = [w for w in text.split()] # This is a list comprehension
+        # Need to create lambda expression outside filter, because filter calls the predicate with one argument
+        pred = lambda w: len(w) > word_length
         # print(list(ft_filter(extract_even, numbers)))
         # print(list(ft_filter(lambda n: n % 2 == 0, numbers)))
-        print(list(ft_filter(lambda n, word_length: len(n) > word_length == 0, word_list)))
+        # print(list(ft_filter(lambda n, word_length: len(n) > word_length == 0, word_list)))
+        print(list(ft_filter(pred, words)))
 
     except AssertionError as error:
         print(F"AssertionError: {error}")
