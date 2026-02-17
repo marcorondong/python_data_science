4d675ce7f879445baf340ac3a2029bbd3d4bbf94
Marco Rondon <marcorondong@hotmail.com>
2026-02-17 20:48:27 +0100

    FIX: Fixed decimal amount in ex01 (no its only 4 decimals)

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 now = dt.datetime.now()
 epoch_seconds = now.timestamp()
-thousand_format = "{:,}".format(epoch_seconds)
+thousand_format = "{:,.4f}".format(epoch_seconds)
 scientific_format = "{:.2e}".format(epoch_seconds)
 
 print(f"Seconds since January 1, 1970: {thousand_format} or {scientific_format} in scientific notation")
